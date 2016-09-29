bf1bd94227499e04ca219c3af2b2ef378dafa93b
Quentin <quentin.grunenwald@hotmail.com>
2016-09-29 19:19:28 +0100

    Added comments

--- league_final.py
+++ league_final.py
@@ -1,6 +1,9 @@
 import csv
 
 def extract_info():
+
+	#Outputs a dictionary with the name of the players as keys and information from the csv file as values
+
     players={}
     with open('soccer_players.csv') as csvfile:
         reader = csv.DictReader(csvfile)
@@ -8,9 +11,11 @@ def extract_info():
             players[player['Name']]=player
     return players
 
-    #Outputs a dictionary with the name of the players as keys and information from the csv file as values
 
 def identify_experienced_players(players):
+
+	#Creates 2 lists of tuples seperating them into experienced and inexperienced players with their name and guardian's name
+
     experienced=[]
     inexperienced=[]
     for player in players:
@@ -21,19 +26,24 @@ def identify_experienced_players(players):
             inexperienced.append((info['Name'],info['Guardian Name(s)']))
     return experienced, inexperienced
             
-    #Creates 2 lists of tuples seperating them into experienced and inexperienced players with their name and guardian's name
 
 def identify_player_requirements(experienced, inexperienced):
+
+	#Identifies number of experienced and inexperienced player per team and rounds to nearest integer
+
     required_experienced = round(len(experienced)/3)
     required_inexperienced = round(len(inexperienced)/3)
     required_total = required_experienced + required_inexperienced
 
     return required_experienced, required_total
 
-    #Look at rounding the number of players required
-    #Identifies number of experienced and inexperienced player per team and rounds to nearest integer
 
 def build_team(team, experienced, inexperienced, required_experienced, required_total):
+
+	#Builds teams based on requirements, populates TEAMS dictionary with players
+    #The tries in this function anticipate a case where the number of players required may 
+    #be rounded, leading to pop an inexistent list object and an IndexError
+
     player_list = []
     players_in_team = 0 
     try:
@@ -50,12 +60,11 @@ def build_team(team, experienced, inexperienced, required_experienced, required_
       pass
     TEAMS[team]=player_list
 
-    #Test this with players_in_team = len(player_list) or even use this in while loop if possible, removes unnecessary variable
-    #Builds teams based on requirements, populates TEAMS dictionary with players
-    #The tries in this function anticipate a case where the number of players required may be rounded, leading to pop an inexistent list object and an IndexError
-
 
 def build_letters(team, player_and_guardians, practice_time):
+
+	#Builds letters
+
     for player in player_and_guardians:
         with open(player[0].lower().replace(' ', '_')+'.txt', 'w') as file:
             file.write(
@@ -67,7 +76,6 @@ def build_letters(team, player_and_guardians, practice_time):
                 'Quentin Grunenwald'.format(player[1], player[0], team, practice_time)
                 )
 
-            #Builds letters
             
 def main():
     players = extract_info()
@@ -76,8 +84,6 @@ def main():
     for team in TEAMS:
         build_team(team, experienced, inexperienced, required_experienced, required_total)
         build_letters(team, TEAMS[team], PRACTICE[team])
-
-        #Implemented alternative to use another constant dicionary for the practice times, seems like it would work
 
 
 if __name__ == '__main__':
